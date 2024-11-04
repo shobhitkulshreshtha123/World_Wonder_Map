a7919b842fc849e0a288a01246b8beca38b139d3
shobhit.kulshreshtha <shobhit.kulshreshtha@gmail.com>
2024-11-04 23:12:26 +0530

    Adjusting the code to Ajax form submission select_wonder routing and iframe embedding

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import json
+from flask import Flask, render_template, request, jsonify
 import folium
-from flask import Flask, request, render_template
+import json
 
 app = Flask(__name__)
 
@@ -10,20 +10,24 @@ with open("wonders_data.json") as f:
 
 @app.route('/')
 def home():
-    return render_template('index.html', wonders=wonders_data.keys())
+    return render_template('index.html')
 
 @app.route('/select_wonder', methods=['POST'])
 def select_wonder():
-    selected_wonder = request.form.get("wonder")
-    if selected_wonder in wonders_data:
-        lat, lon = wonders_data[selected_wonder]
-        # Create map centered on selected wonder
-        wonder_map = folium.Map(location=[lat, lon], zoom_start=6, tiles="OpenStreetMap")
-        # Add marker for the selected wonder
-        folium.Marker([lat, lon], popup=selected_wonder, tooltip=selected_wonder).add_to(wonder_map)
-        # Save map to HTML file
-        wonder_map.save("templates/wonder_map.html")
-    return render_template('wonder_map.html')
+    data = request.get_json()
+    selected_wonder = data['wonder']
+    
+    # Get coordinates for the selected wonder
+    coords = wonders_data.get(selected_wonder)
+    if coords:
+        lat, lon = coords
+        wonder_map = folium.Map(location=[lat, lon], zoom_start=6)
+        folium.Marker(location=[lat, lon], popup=selected_wonder).add_to(wonder_map)
+        map_html = wonder_map._repr_html_()  # Generate HTML representation of the map
+        
+        return jsonify({'map_html': map_html})
+    else:
+        return jsonify({'error': 'Wonder not found'}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
